c6aa34445da67b71d7ed4c40ddbf98c22225b047
Akanksha Borkar <akanksha.borkar4@gmail.com>
2026-08-27 02:26:53 +0530

    Modify author and enhance comments in band name generator
    Updated author name and added example output in comments.

---
+++ script/Band name generator.py
@@ -0,0 +1,38 @@
+
+# --------------------------------------------------
+# Project: Band Name Generator
+# Author: Akanksha Borkar
+# Repository: Python Mini Projects
+# Description:
+# A simple Python program that generates a band name
+# using user-provided inputs.
+#
+# Skills Practiced:
+# - User Input
+# - Variables
+# - String Concatenation
+#
+## Band Name Generator
+
+#Generates a fun band name based on user input.
+
+### Example Output
+
+#```text
+#Welcome to the Band Name Generator!
+
+#Whats your city name ?
+#Mumbai
+
+#Whats your pet's name?
+#Rocky
+
+#Your band name could be: Mumbai Rocky
+#```
+
+print("hello " + input("whats your name?") + "!")
+petname = input("whats your pet's name?")
+cityname = input("whats your city's name?")
+print("Your band name is:- " + petname + " " + cityname)
+
+
